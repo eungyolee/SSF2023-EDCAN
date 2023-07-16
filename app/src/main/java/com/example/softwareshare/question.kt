956a4fc9f04aef18c20de450d5c81c9651dae53c
hoonih <hoonih1004@gmail.com>
2023-07-17 01:45:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/softwareshare/question.kt
@@ -0,0 +1,11 @@
+package com.example.softwareshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class question : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
